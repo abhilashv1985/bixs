5ec28899492ba970126bf7ed9187d5023e5a8c04
Abhilash V <blueeyes4ya@gmail.com>
2014-03-07 23:44:22 +0530

    Sorting order of module fields - view

--- application/views/pages/showModuleContent.php
+++ application/views/pages/showModuleContent.php
@@ -6,10 +6,16 @@
  
    $( "div.sectionDiv" ).sortable({
         cancel: ".SectionHead",
-        stop: function (event, ui) {
-            var data = $(this).sortable('serialize');
+        update: function (event, ui) {
+            var stringDiv = "";
+            $("div.sectionDiv:first").children('.fieldContent').each(function(i) {
+                var div = $(this);
+                stringDiv += " "+div.data("id") + '=' + div.data("sort") + '&';
+            });            
+            //alert(stringDiv);
+            //var data = $(this).sortable('serialize');
             $.ajax({
-                data: data,
+                data: stringDiv,
                 type: 'POST',
                 url: base_url+'module/updateFieldPosition'
             });
@@ -36,14 +42,14 @@
                 ?>       
                     </div>     
                     <div class="sectionDiv">  
-                        <div class="fieldContent" id="item-<?php echo $count; ?>">
+                        <div class="fieldContent" id="item-<?php echo $count; ?>" data-id="<?php echo $content->meta_fieldid;?>" data-sort="<?php echo $count; ?>">
                            <?php echo $content->field_name;?>       
                             <input type="hidden" value="<?php echo $content->id;?>" name="item-<?php echo $count; ?>"/>
                        </div>
                 <?php
                 } else{
                     ?>
-                    <div class="fieldContent" id="item-<?php echo $count; ?>">
+                    <div class="fieldContent" id="item-<?php echo $count; ?>" data-id="<?php echo $content->meta_fieldid;?>" data-sort="<?php echo $count; ?>">
                         <?php echo $content->field_name;?>       
                         <input type="hidden" value="<?php echo $content->id;?>" name="item-<?php echo $count; ?>"/>
                     </div>
