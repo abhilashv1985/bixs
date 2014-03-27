eac510da9e9df35e86f3a8c85840e3e51fa6ec4b
Abhilash V <blueeyes4ya@gmail.com>
2014-03-27 07:33:22 +0530

    check if element exists

--- js/module.js
+++ js/module.js
@@ -1,16 +1,10 @@
 $(document).ready(function() {
-    $(document).on('click', '.date', function(e) {
-        alert("date clickedddddddddd");
-    });
-    $(".date").datepicker({
-        changeMonth: true,
-        changeYear: true,
-        yearRange: "2012:2016",
-        altFormat: "yy-mm-dd"
-    });
-    
+    if($(".date").length){
+        $(".date").datepicker({
+            changeMonth: true,
+            changeYear: true,
+            yearRange: "2012:2016",
+            altFormat: "yy-mm-dd"
+        });        
+    }
 });
-
- $(document).on('click', '.date', function(e) {
-        alert("date clickedddddddddd");
-    });
